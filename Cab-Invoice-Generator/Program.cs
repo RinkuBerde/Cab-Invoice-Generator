8d771e560a7a6c06d4f7fe71351a80f70d22e018
rinkuberde <rinkuberde@gmail.com>
2023-02-10 12:38:00 +0530

    [Rinku] -Welcome Message

---
+++ Cab-Invoice-Generator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Cab_Invoice_Generator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Cab-Invoice-Generator Problem!!!");
+        }
+    }
+}
